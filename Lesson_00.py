359bf6ab535a2a15093ebc867cb0577013f09699
Cycil <coolgoldenfreddy19@gmail.com>
2024-09-16 10:52:51 -0400

    but why?

---
+++ Lesson_00.py
@@ -0,0 +1,27 @@
+#Step-1 create a new file with snake case.
+print("cycil")
+#Step-2 print my name into the console.
+Favshow = "Gurren Laggan"
+#step-3 use snake case to make a variable named "fav show".
+Integer = 31
+Float = 3.1
+#Step-4 using snake case create one float and one integer variable.
+print(Integer * Float)
+#step-5 print the product of the two previuse variables.
+Favfood = ("Skittes", "Chicken", "Fish")
+#step-6 make a variable that stores your fav food (at least 3).
+def drivers_license(age):
+    if age >= 16:
+        return "You are eligible to get a driver's license."
+    else:
+        return "You are not eligible to get a driver's license yet."
+#step-7 make a if statement that tells someone if tbat a get drivers license.
+name = input("what is your name?")
+print(name)
+#step-8 ask user for their name and print to the consle.
+for x in Favfood:
+  print(x) 
+#step-9 make a loop that prints each of your fav foods.
+while Integer * float == 3:
+ float = 5
+#step-10 make a wild loop that will never run
