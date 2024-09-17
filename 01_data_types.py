0388aa42c7e49860ef8a41199cfbd5b446187c94
Cycil <coolgoldenfreddy19@gmail.com>
2024-09-17 10:51:45 -0400

    but why? x2

---
+++ 01_data_types.py
@@ -0,0 +1,49 @@
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+number1 = 12.34
+number2 =int(number1) # Changes number1 into a whole number
+print (number1)
+print (number2)
+
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+putnum = int(input("Put an positive number, negative number, or zero"))
+print (putnum)
+
+if putnum > 0: 
+    print ("positive")
+elif putnum < 0: 
+    print ("negative")
+else:
+    print ("zero")
+
+
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+
+
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
