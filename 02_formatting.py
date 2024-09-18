7951cafa8353f72cdfe983f44dabf7ce6d205ac2
Cycil <coolgoldenfreddy19@gmail.com>
2024-09-18 10:50:20 -0400

    day 2

---
+++ 02_formatting.py
@@ -0,0 +1,44 @@
+"""
+TASK 1:
+Write code that checks if a user entered the correct password.
+The password should not be case sensitive
+
+"""
+
+
+"""
+TASK 2:
+Write code that checks if a user inputs an empty string
+If the string is empty, print "invalid" otherwise print "valid"
+"""
+words = input("put some words in ;3")
+if no
+
+"""
+TASK 3:
+
+Write a program that will replace the word "cat" with the word "dog"
+It should replace all occurances regardless of captilization 
+"""
+pet1 = 'cat'
+new_pet = pet1.replace('cat','dog')
+print (new_pet)
+
+"""
+TASK 4:
+
+Write a program that takes a person's name and age as input and prints it
+"""
+age = input("what is your age?")
+print(age)
+name = input("what is your name?")
+print(name)
+
+
+
+"""
+TASK 5:
+
+Write a program that takes in two floats, and prints the quotient
+The result should be rounded to the nearest tenth (1 decimal place)
+"""
