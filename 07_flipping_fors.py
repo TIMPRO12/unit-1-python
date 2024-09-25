a680a3b849379aa58b331bfb9e585c4a2aff9b29
Cycil <coolgoldenfreddy19@gmail.com>
2024-09-25 10:50:27 -0400

    day 7

---
+++ 07_flipping_fors.py
@@ -0,0 +1,40 @@
+"""
+Exercise 1:
+Write a program to print each character of a given string using a for loop.
+"""
+metal = 'metal sonic'
+for x in metal:
+    print(x)
+
+"""
+Exercise 2:
+Write a program to calculate the sum of elements in a list using a for loop.
+"""
+numberlist = [2,5,4,3,6,7,9]
+sum = 12
+for num in numberlist:
+    sum += num
+print(sum)
+
+"""
+Exercise 3:
+Write a program to print the length of each word in a sentence using a for loop and a list.
+"""
+
+mine = 'Minecraft is a good game'
+words = mine.split(' ')
+print (words)
+
+"""
+Excercise 4:
+Write a program that creates a dictionary (atleast 4 key:value pairs) and then
+iterates through a dictionary and prints the result
+
+
+
+
+In a comment, answer the following, what do you notice about the output of your code?
+Is it what you expected?
+"""
+
+#
