736bbb6955c714330b524482b67680fa5baafa14
TIMPRO12 <coolgoldenfreddy19@gmail.com>
2024-09-26 20:41:42 -0400

    ToDo list

--- Aoi Todo.py
+++ Aoi Todo.py
@@ -1,9 +1,49 @@
 print ('To-do List!')
 print ('')
 print ('')
-todo_list = ['get cash', 'spend cash', 'cry']
-print (todo_list)
+todo_list = []
 
-add = input('what task would you like to add?')
-if add == 
+while 1:
+    print('What would you like to do?')
+    print("")
+    print('1. Add a Todo')
+    print('2. View Task')
+    print('3. Snap Away a Todo')
+    print('4. Burn It All!')
+    print("")
+    print('Enter one of one of the numbers above.')
+    pick = int(input(""))
+    print("")
+
+    num = 1
+    if pick == 2:
+        for thing in todo_list:     
+            print(f"{num}){thing}")
+            num += 1
+        print("")
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("")
+    elif pick == 1:
+        print('What Would you like to add?')
+        add = input("")
+        todo_list.append(add)
+        print("")
+        print(f"Added {add} To Your ToDo List.")
+        print("")
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("")
+    elif pick == 3:
+        print('What Would You Like To Remove?')
+        remove = input("")
+        todo_list.remove(remove)
+        print(f"Removed {remove} From Your ToDo List.")
+        print("")
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("")
+    elif pick == 4:
+        todo_list = []
+        print('Your ToDo list Is Cleared')
+    else:
+        print('Nuh Uh.')
+
 
