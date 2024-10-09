75a3605e6770d7aa67270130f9050996d2989b34
Cycil <coolgoldenfreddy19@gmail.com>
2024-10-09 10:50:31 -0400

    day 12

---
+++ 12_functions.py
@@ -0,0 +1,56 @@
+import math
+"""
+Task 1: Greeting
+Write a function that takes a name as a 
+parameter and prints a greeting message like "Hello, [name]!".
+"""
+def my_function(name): 
+    print("Hello " + name)
+
+my_function("Logan")
+
+
+"""
+Task 2: Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+def square():
+    int(input("input and number"))
+
+    print()
+
+
+
+"""
+Task 3: Even or Odd
+Write a function that takes a number as a argument that 
+returns `True` if the number is even, and `False` otherwise.
+"""
+
+
+
+
+"""
+Task 4: Area of a Rectangle
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+
+
+"""
+Task 5: Celsius to Fahrenheit
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+
+
+"""
+Task 6: Average of Numbers
+Write a function that takes a list of numbers 
+and returns the average (mean) of those numbers.
+"""
+
+"""
+Task 7: Total Calculator
+Create a function that has arguments for the price and quantity of something, and returns the total.
+Your function should also optionally accept a 3rd argument for discount, and return the discounted if provided.
+"""
